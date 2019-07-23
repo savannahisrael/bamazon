43b306ea600c6474521e61667fc717f4c51d836f
Savannah Israel <sisrael0405@gmail.com>
2019-07-22 18:16:46 -0600

    changed function structure

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,36 +9,67 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-function viewInventory() {
-    connection.query("SELECT * FROM products", function (err, data) {
+
+
+function menu() {
+    connection.query("SELECT * FROM products", function(err, data) {
         if (err) throw err;
 
         console.table(data);
-        menu();
-    })
-}
-
-function menu() {
+    
     inquirer.prompt([
         {
-        message: "What is the Item ID number of the item you want to purchase?",
-        type: "input",
-        name: "item",
-        },
-        {
-        message: "How many units of the product do you want to buy?",
-        type: "input",
-        name: "units"
+        message: "What is the ID of the product you would like to buy?",
+        type: "list",
+        name: "productName",
+        choices: function () {
+            var options = data.map(function(option) {
+                return option.product_name;
+            });
+            return options;
         }
-    ]).then(function(answers) {
-        
-    })
+    },
+    {
+        message: "How much would you like to purchase?",
+        name: "amount",
+        validate: function(input) {
+            if(isNaN(input)) {
+                return "Please enter a number to purchase";
+            }
+            return true;
+        }
+    }
+]).then(function(answer) {
+        // console.log(data);
+        var selectedItem;
+        for(var i = 0; i < data.length; i++){
+            if(data[i]["product_name"] === answer.productName){
+                selectedItem = data[i]
+                // console.log(selectedItem);
+            }
+            
+        }
+        if(selectedItem.stock_quantity < parseInt(answer.amount)) {
+            console.log("Insufficient quantity!");
+        } else {
+
+            connection.query("UPDATE products SET ? WHERE ?", [{
+                stock_quantity: selectedItem.stock_quantity - parseInt(answer.amount)
+            },
+            {
+                product_name: answer.productName
+            }], function(err) {
+                if (err) throw err;
+                 console.log(`You have purchased ${answer.amount} of ${answer.productName}!`);
+
+                 var purchasePrice = answer.amount * selectedItem.price
+                 console.log(`Your purchase price is ${purchasePrice}`);
+
+            }
+            )
+        };
+    });
+})
 }
 
-
-connection.connect(function(err) {
-    if (err) throw err;
-
-    console.log("Welcome valued customer!  See our current inventory below:")
-    viewInventory();
-});
+menu();
